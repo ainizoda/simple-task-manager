000433fbe8f39c431ea9ab96f368e50651af38f0
ainizoda <ainizodam@gmail.com>
2024-11-08 02:44:12 +0500

    minor change

--- pkg/menu/types.go
+++ pkg/menu/types.go
@@ -3,9 +3,9 @@ package menu
 type MenuOption int
 
 const (
-	Exit   MenuOption = 0
-	First  MenuOption = 1
-	Second MenuOption = 2
-	Third  MenuOption = 3
-	Fourth MenuOption = 4
+	Exit MenuOption = iota
+	First
+	Second
+	Third
+	Fourth
 )
